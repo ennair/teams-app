c2123ecdbf1c649179e240c9a87fbd471d74f7bf
Rianne Nieland <rianne.nieland@capgemini.com>
2020-12-17 15:03:00 +0100

    Updates

---
+++ precache-manifest.3012400e01a5097194351acea61c670e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b154521ff177e9871667280d807afe0",
+    "url": "/teams-app/index.html"
+  },
+  {
+    "revision": "15dd542bf66bef4f1bed",
+    "url": "/teams-app/static/css/main.4a1b6a88.chunk.css"
+  },
+  {
+    "revision": "75b5178ee82229aa02e6",
+    "url": "/teams-app/static/js/2.f89610ce.chunk.js"
+  },
+  {
+    "revision": "4c98c6b556b8cc39dae85626e0284631",
+    "url": "/teams-app/static/js/2.f89610ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15dd542bf66bef4f1bed",
+    "url": "/teams-app/static/js/main.4a0491f0.chunk.js"
+  },
+  {
+    "revision": "360dc42c21a42c603ece",
+    "url": "/teams-app/static/js/runtime-main.9a5c9c45.js"
+  }
+]);
